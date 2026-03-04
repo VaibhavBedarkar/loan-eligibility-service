e697bbabc635a745727765c0836b19ff9dd10d0b
vaibhavbedarkar <vaibhav.bedarkar98@gmail.com>
2026-03-04 11:52:12 +0530

    [Added]: Initial Project Setup

---
+++ DEVELOPMENT_NOTES.md
@@ -0,0 +1,13 @@
+# Loan Application Evaluation Service
+
+### Introduction
+
+### Overall approach
+
+### Key design decisions 
+
+### Trade-offs considered
+
+### Assumptions made 
+
+### Proposed Improvements  
---
+++ src/test/java/com/rbih/LoanEligibilityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rbih;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanEligibilityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
